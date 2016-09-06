ce860ba0989738ec9bace3809026ad8c6f25adb2
Edjan <edjansantos@gmail.com>
2016-09-06 00:57:16 -0300

    Vereadores and Prefeito pages, temp favicon added

--- app/sp-eleicoes.js
+++ app/sp-eleicoes.js
@@ -8,6 +8,12 @@
         }).state("app.home", {
             "url": "/",
             "templateUrl": "modules/common/views/home.html"
+        }).state("app.vereadores", {
+            "url": "/vereadores",
+            "templateUrl": "modules/common/views/vereadores.html"
+        }).state("app.prefeito", {
+            "url": "/prefeito",
+            "templateUrl": "modules/common/views/prefeito.html"
         });
 
         $locationProvider.html5Mode(true);
